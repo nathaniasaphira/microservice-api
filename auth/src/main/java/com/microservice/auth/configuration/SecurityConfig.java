dc1dbf686f4db0887183da67c928076d9368d406
Nathania Saphira <nathaniasaphira@gmail.com>
2024-06-04 16:46:55 +0800

    refactor: remove auth-app

--- auth/src/main/java/com/microservice/auth/configuration/SecurityConfig.java
+++ auth/src/main/java/com/microservice/auth/configuration/SecurityConfig.java
@@ -2,10 +2,8 @@ package com.microservice.auth.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
